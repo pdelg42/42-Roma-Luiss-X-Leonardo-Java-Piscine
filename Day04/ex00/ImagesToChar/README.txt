db6938937e99587d3bfabdd8a4021dddb0cb984c
Andrea <aperrone@e3r3p5.42roma.it>
2023-04-24 19:13:07 +0200

    ex0401

--- Day04/ex00/ImagesToChar/README.txt
+++ Day04/ex00/ImagesToChar/README.txt
@@ -1,2 +1,2 @@
 javac -d ./target -sourcepath ./src/java ./src/java/edu/school42/printer/app/Program.java
-java -cp ./target /Program
+java -cp ./target edu.school42.printer.app.Program . 0 /Users/aperrone/Desktop/42-Roma-Luiss-X-Leonardo-Java-Piscine/Day04/ex00/ImagesToChar/it.bmp
--- Day04/ex00/ImagesToChar/src/java/edu/school42/printer/app/Program.java
+++ Day04/ex00/ImagesToChar/src/java/edu/school42/printer/app/Program.java
@@ -1,6 +1,25 @@
+package edu.school42.printer.app;
+
+import edu.school42.printer.logic.Printer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Program {
-    
-    public static void main(String[] args) {
-        System.out.println("");
-    }
+	private static BufferedImage image;
+
+	public static void main(String[] args) {
+		if (args.length != 3 || (args[0].length() > 1 ) || (args[1].length() > 1)) {
+			System.out.println("Input Error!");
+			System.exit(-1);
+		}
+		try {
+			image = ImageIO.read(new File(args[2]));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		new Printer(args[0].charAt(0), args[1].charAt(0)).printImage(image);
+	}
 }
